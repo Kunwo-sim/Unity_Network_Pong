008bb4e109b6013636aecf437f80d231216358f9
SKW <rjsdntlsdj@gmail.com>
2021-10-03 05:28:39 +0900

    [Update] Lobby Scene

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -11,31 +11,50 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     
     private void Start()
     {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
 
+        joinButton.interactable = false;
+        connectionInfoText.text = "Connecting To Master Server...";
     }
     
     public override void OnConnectedToMaster()
     {
-        
+        joinButton.interactable = true;
+        connectionInfoText.text = "Online : Connected to Mater Server";
     }
     
     public override void OnDisconnected(DisconnectCause cause)
     {
-    
+        joinButton.interactable = false;
+        connectionInfoText.text = $"Offline : Connection Disabled {cause.ToString()} - Try reconnecting...";
+        PhotonNetwork.ConnectUsingSettings();
     }
     
     public void Connect()
     {
-        
+        joinButton.interactable = false;
+        if(PhotonNetwork.IsConnected)
+        {
+            connectionInfoText.text = "Connecting to Random Room...";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            connectionInfoText.text = "Offline : Connection Disabled - Try reconnecting...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
     
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-    
+        connectionInfoText.text = "There is no empty room, Creating new Room.";
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2});
     }
     
     public override void OnJoinedRoom()
     {
-    
+        connectionInfoText.text = "Connected with Room.";
+        PhotonNetwork.LoadLevel("Main");
     }
 }
